81f15bc6b6f99c69cf20124548575ab714219c89
duraes-antonio <garotoseis@gmail.com>
2019-07-13 10:45:41 -0300

    Style: Barra de busca já estilizada e com botões

--- frontend/src/app/componentes/componentes.module.ts
+++ frontend/src/app/componentes/componentes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import {BarraBuscaComponent} from './barra-busca/barra-busca.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, BarraBuscaComponent],
   exports: [
     NavbarComponent
   ],
