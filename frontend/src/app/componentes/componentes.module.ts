f869f1d6fa4d5570243406f350718201e0729c58
duraes-antonio <garotoseis@gmail.com>
2019-07-15 04:18:41 -0300

    NEW: Barra de busca - OK, Banner, Navbar -Barra de busca já estilizada e funcional; -Navbar responsosivo (p/ larguras > 340); -Início da construção do banner;

--- frontend/src/app/componentes/componentes.module.ts
+++ frontend/src/app/componentes/componentes.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import {BarraBuscaComponent} from './barra-busca/barra-busca.component';
+import {FormsModule} from '@angular/forms';
+import {BannerComponent} from './banner/banner.component';
 
 @NgModule({
-  declarations: [NavbarComponent, BarraBuscaComponent],
+  declarations: [NavbarComponent, BarraBuscaComponent, BannerComponent],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    BannerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class ComponentesModule { }
--- frontend/src/app/componentes/navbar/navbar.component.ts
+++ frontend/src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +12,22 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  public exibirBusca: boolean;
+  public aplicarFadeOut: boolean;
+
   ngOnInit() {
+    this.exibirBusca = false;
+    this.aplicarFadeOut = true;
   }
 
+  /**
+   * Oculta a barra de busca se já estiver sendo exibida, senão, exibe-a.
+   */
+  public toggleBusca() {
+
+    /*Se a barra já está sendo exibida, oculte-a.
+      Se a barra está oculta, exiba-a*/
+    this.exibirBusca = !this.exibirBusca;
+    this.aplicarFadeOut = !this.aplicarFadeOut;
+  }
 }
