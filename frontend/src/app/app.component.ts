6de7b9d36f568f931ec0413d29abca09322f09e1
duraes-antonio <garotoseis@gmail.com>
2019-07-19 03:30:13 -0300

    UP: Finalização do rodapé

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent} from './componentes/navbar/navbar.component';
+import {ListaLinks} from "./modelos/ListaLinks";
+import {Link} from "./modelos/Link";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,31 @@ import { NavbarComponent} from './componentes/navbar/navbar.component';
 })
 export class AppComponent {
   title = 'frontend';
+  listaLinks: Array<ListaLinks>;
+
+  constructor() {
+    this.listaLinks = new Array<ListaLinks>();
+
+    let links_1 = ["Facebook", "Twitter", "Zipzop", "Instagram", "Orkut", "MSN"];
+    let links_2 = ["Angular 8", "Node JS 10", "C#", "MongoDB", "Python 3.6",
+      "PostgreSQL 11", "Materialize CSS 1.0", "Docker"];
+
+    let links_obj_1: Array<Link> = new Array<Link>();
+    let links_obj_2: Array<Link> = new Array<Link>();
+
+    for (let link of links_1) {
+      links_obj_1.push(new Link(link, "www.google.com"));
+    }
+
+    for (let link of links_2) {
+      links_obj_2.push(new Link(link, "www.google.com"));
+    }
+
+    this.listaLinks.push(new ListaLinks("Redes sociais", links_obj_1));
+    this.listaLinks.push(new ListaLinks("Tecnologias", links_obj_2));
+    this.listaLinks.push(new ListaLinks("Tecnologias", links_obj_2));
+    this.listaLinks.push(new ListaLinks("Tecnologias", links_obj_2));
+    this.listaLinks.push(new ListaLinks("Redes sociais", links_obj_1));
+
+  }
 }
--- frontend/src/app/componentes/rodape/rodape.component.ts
+++ frontend/src/app/componentes/rodape/rodape.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ListaLinks} from "../../modelos/ListaLinks";
 
 @Component({
   selector: 'app-rodape',
@@ -7,10 +8,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RodapeComponent implements OnInit {
 
+  @Input() links: Array<ListaLinks>;
+
   constructor() {
   }
 
   ngOnInit() {
+    window.addEventListener('load', this.ajustarTamanhoColunas);
+    window.addEventListener('resize', this.ajustarTamanhoColunas);
   }
 
+  private ajustarTamanhoColunas() {
+
+    let listaCols = document.getElementById("lista-cols");
+    let rodape = document.getElementById("rodape");
+    let cols = document.getElementsByClassName("col-links-important");
+
+    let soma: number = 0;
+
+    for (let i = 0; i < cols.length; ++i) {
+      soma += cols[i].clientWidth;
+    }
+
+    if (soma < rodape.clientWidth) {
+      listaCols.classList.add("lista-col-links");
+    } else {
+      listaCols.classList.remove("lista-col-links");
+    }
+  }
 }
