87c5b37a35c398f8fff1a841787cb5a5ff9f0e7e
duraes-antonio <garotoseis@gmail.com>
2019-08-03 05:10:22 -0300

    Teste deploy

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Link} from "./modelos/Link";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'frontend';
