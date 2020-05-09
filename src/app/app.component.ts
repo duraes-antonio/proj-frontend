b4444837188692147784dcb12c5a01e596b395a9
duraes-antonio <garotoseis@gmail.com>
2020-05-09 01:24:56 -0300

    FIX: Preenchimento de toda altura da janela e alinhamento do conteúdo ao topo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {ListLink} from './models/componentes/list-link';
 import {AuthService} from './services/auth.service';
 import {ListLinkService} from './services/lists/list-link.service';
@@ -8,16 +8,27 @@ import {ListLinkService} from './services/lists/list-link.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'proj-frontend';
   listLinks: ListLink[] = [];
   sidenavShow = false;
   authServRef = AuthService;
+  readonly pseudoBodyId = 'body-content';
 
   constructor(private _listLinkServ: ListLinkService) {
     AuthService.loadFromLocalToken();
     _listLinkServ.get().subscribe(
       (lists: ListLink[]) => this.listLinks = lists
     );
+  }
+
+  ngAfterViewInit(): void {
+    const body = document.getElementById(this.pseudoBodyId) as HTMLElement;
+    const headerHeight = document.getElementById('navbar')?.clientHeight;
+    const footerHeight = document.getElementById('footer')?.clientHeight;
+    if (body) {
+      const minSize = window.innerHeight - 1 - (headerHeight ?? 0) - (footerHeight ?? 0);
+      body.style.minHeight = `${minSize}px`;
+    }
   }
 }
