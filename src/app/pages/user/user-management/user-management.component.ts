1e535b4cebafa2044d65f989fe6cfe6625b1962b
duraes-antonio <garotoseis@gmail.com>
2020-05-24 14:36:04 -0300

    NEW: Listagem de usuários finalizada

--- src/app/pages/user/user-management/user-management.component.ts
+++ src/app/pages/user/user-management/user-management.component.ts
@@ -1,7 +1,20 @@
 import {Component} from '@angular/core';
 import {UserService} from '../../../services/user.service';
 import {Observable} from 'rxjs';
-import {User} from '../../../models/user';
+import {UserSearch} from '../../../models/user';
+import {debounceTime, distinctUntilChanged, filter, switchMap} from 'rxjs/operators';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {AbstractControl, FormControl, FormGroup} from '@angular/forms';
+import {FilterUser} from '../../../models/filters/filter-user';
+import {UserOptionsSort} from '../../../enum/user';
+import {validators} from '../../../../shared/validations/validatorsCustom';
+import {fmtDate} from '../../../../shared/constants/formats';
+import {userConstants} from '../../../../shared/constants/objects/user';
+import {userSizes} from '../../../../shared/constants/field-size';
+import {getMsgFront} from '../../../../shared/validations/msgErrorFunctionsFront';
+import {utilDOM} from '../../../../shared/util.dom';
+import {AuthService} from '../../../services/auth.service';
+import {ERole} from '../../../enum/roles';
 
 @Component({
   selector: 'app-user-management',
@@ -10,9 +23,94 @@ import {User} from '../../../models/user';
 })
 export class UserManagementComponent {
 
-  users$: Observable<User[]>;
+  readonly filter: FilterUser = {
+    currentPage: 1,
+    perPage: 10,
+    sortBy: UserOptionsSort.NAME
+  };
+  readonly formGroup = new FormGroup({
+    dateStart: new FormControl(null, validators.dateValidator(false)),
+    dateEnd: new FormControl(null, validators.dateValidator(false)),
+    roles: new FormControl([]),
+    text: new FormControl(''),
+  });
+  readonly fmtDate = fmtDate;
+  readonly sizes = userSizes;
+  readonly userConsts = userConstants;
+  readonly _getMsgError = getMsgFront;
+  readonly _currentUserId = AuthService.userLogged?.id;
+  userSearchResult?: UserSearch;
+  requesting = false;
+  sortBy = UserOptionsSort.NAME;
 
-  constructor(private readonly _userServ: UserService) {
-    this.users$ = this._userServ.get();
+  constructor(
+    private readonly _userServ: UserService,
+    private readonly _loadingServ: NgxSpinnerService
+  ) {
+    _loadingServ.show();
+    const textChanges$ = this.formGroup.controls['text'].valueChanges
+      .pipe(
+        debounceTime(250),
+        filter(value => value.length > 1),
+        distinctUntilChanged(),
+        switchMap(text => {
+          this.showLoading();
+          return this._userServ.get(
+            this._updateFilter(this.filter, {...this.formGroup.value, sortBy: this.sortBy, text})
+          );
+        })
+      );
+    this._extractSearch(textChanges$);
+    this._extractSearch(this._userServ.get(this.filter));
+  }
+
+  maskDate(event: Event, control: AbstractControl) {
+    utilDOM.maskDate(event, control);
+  }
+
+  searchUsers(targetPage?: number) {
+    const request = this._userServ.get(
+      this._updateFilter(
+        this.filter,
+        {
+          ...this.formGroup.value,
+          sortBy: this.sortBy,
+          currentPage: targetPage ?? this.filter.currentPage
+        })
+    );
+    this._extractSearch(request);
+  }
+
+  showLoading() {
+    this.requesting = true;
+    this.formGroup.disable();
+  }
+
+  hideLoading() {
+    this.requesting = false;
+    this.formGroup.enable();
+  }
+
+  changePageSearch(targetPage: number) {
+    this.searchUsers(targetPage);
+  }
+
+  updateRoles(userId: string, roles: ERole[], role: ERole | string) {
+    const newRoles = roles.includes(role as ERole)
+      ? roles.filter(r => r !== role)
+      : [...roles, role] as ERole[];
+    this._userServ.patchRoles(userId, newRoles).subscribe(() => this.searchUsers());
+  }
+
+  private _updateFilter(currentFilter: FilterUser, paramsFilter: object): FilterUser {
+    return {...currentFilter, ...paramsFilter};
+  }
+
+  private _extractSearch(request: Observable<UserSearch>) {
+    this.showLoading();
+    request.subscribe(result => {
+      this.userSearchResult = result;
+      this.hideLoading();
+    });
   }
 }
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -2,13 +2,26 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UserManagementComponent} from './user-management/user-management.component';
 import {UserRoutingModule} from './user-routing.module';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {ComponentesModule} from '../../components/componentes.module';
 
 
 @NgModule({
   declarations: [UserManagementComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    NgxSpinnerModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    ComponentesModule
   ]
 })
 export class UserModule {
