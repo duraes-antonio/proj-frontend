39e5100964a8f17a6da055e703d3d472fbaf8a27
duraes-antonio <garotoseis@gmail.com>
2020-05-24 14:35:31 -0300

    NEW: Método p/ patch de roles em serviço de usuários

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,10 +3,11 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {AuthService, LoginReturn} from './auth.service';
-import {tap} from 'rxjs/operators';
-import {User, UserAdd} from '../models/user';
+import {take, tap} from 'rxjs/operators';
+import {User, UserAdd, UserSearch} from '../models/user';
 import {httpService} from './generic-http.service';
 import {FilterUser} from '../models/filters/filter-user';
+import {ERole} from '../enum/roles';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,16 @@ export class UserService {
   constructor(private readonly _http: HttpClient) {
   }
 
-  get(filter?: FilterUser): Observable<User[]> {
-    return httpService.get(this._endpoint, this._http, AuthService.getHeaders, filter);
+  get(filter?: FilterUser): Observable<UserSearch> {
+    return httpService.getSingle(`${this._endpoint}/search`, this._http, AuthService.getHeaders, filter);
+  }
+
+  patchRoles(userId: string, roles: ERole[]): Observable<User> {
+    return this._http.patch<User>(
+      `${this._endpoint}/${userId}/roles`,
+      roles,
+      {headers: AuthService.getHeaders()}
+    ).pipe(take(1));
   }
 
   post(user: UserAdd): Observable<LoginReturn> {
