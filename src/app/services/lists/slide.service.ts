ed1431182454a15d796e1ffaeddb9853e0b4ebd3
duraes-antonio <garotoseis@gmail.com>
2020-06-12 05:36:28 -0300

    NEW: Serviço para slider

---
+++ src/app/services/lists/slide.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
+import {httpService} from '../generic-http.service';
+import {Slide, SlideAdd} from '../../models/componentes/slide';
+import {take} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SlideService {
+
+  private readonly _endpoint = `${environment.apiUrl}/slide`;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  delete(slideId: string): Observable<void> {
+    return httpService.delete(this._endpoint, slideId, this._http, AuthService.getHeaders);
+  }
+
+  patch(slidePatch: object, id: string): Observable<Slide> {
+    return httpService.patch(this._endpoint, id, this._http, AuthService.getHeaders, slidePatch);
+  }
+
+  patchImage(image: File, id: string): Observable<Slide> {
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    return this._http.patch<Slide>(
+      `${this._endpoint}/${id}/image`,
+      formData,
+      {
+        headers: AuthService.getHeaders().delete('Content-Type'),
+      }
+    ).pipe(take(1));
+  }
+
+  post(slideAdd: SlideAdd): Observable<Slide> {
+    return httpService.post(this._endpoint, this._http, AuthService.getHeaders, slideAdd);
+  }
+}
